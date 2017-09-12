5fc3829627e98f9e0729377ea0a8dd27d39afb7c
The-Puppeteer <dlhaselgrove@willamette.edu>
2017-09-12 08:46:05 -0700

    first commit

---
+++ circle.js
@@ -0,0 +1,64 @@
+var canvas; //init canvas variable
+var rect; //init rect variable
+var ctx; //init context variable
+var clout = []; //init new global array
+var eAngle = 0; //init start angle for cirlce
+var sAngle = 2*Math.PI; //init end angle for circle
+
+window.onload = function init(){ //begins during the loading of the page
+    
+    canvas = document.getElementById("swag"); //reads in canvas from html page
+    rect = canvas.getBoundingClientRect(); //sets rectangle to canvas size
+    ctx = canvas.getContext("2d"); //sets the context for the canvas
+
+    canvas.addEventListener("click", function(event) { // adds a click event listener for the canvas
+    	var circle = new Circle(event.x - rect.left, event.y - rect.top, Math.random()*30, "black"); //creates new local variable circle and sets x & y from mouse pos, r to a random int*30 and the color to black
+    	clout.push(circle); // pushes the new circle onto the array
+    	circle.drawCirc(); //calls the drawCirc method and, well, draws the new circle
+    	if(canvas.oncontextmenu = function(){ //when the user right clicks, call the ranColor() method and revent the RC menu 
+    		ranColor();
+    		return false;
+    	});
+    	   
+	 });
+
+    document.getElementById('clear').addEventListener('click', function() { //button stuff. essentially clears every point on the canvas and 
+        ctx.clearRect(0, 0, canvas.width, canvas.height);//clears the array to completely clear the canvas
+        clout = [];
+      }, false);
+
+};
+
+function Circle (x, y, r, color) { //defines circle object
+	this.x = x;
+	this.y = y;
+	this.r = r;
+	this.color = color;
+}
+
+Circle.prototype.drawCirc = function() { //method that draws the circle on the canvas
+	ctx.beginPath();
+	ctx.arc(this.x, this.y, this.r, eAngle, sAngle);
+	ctx.fillStyle = this.color;
+	ctx.fill();
+	ctx.stroke;	
+};
+
+function ranColor(){ // creates a random color and sets a new color to each ith circle
+	for(i = 0; i < clout.length; i++){
+		clout[i].color = "rgb("+
+    	Math.floor(Math.random()*256)+","+
+    	Math.floor(Math.random()*256)+","+
+    	Math.floor(Math.random()*256)+")";
+    	clout[i].drawCirc();
+	}	
+}
+
+
+
+
+
+
+
+
+
